71de559721253c9299f411ae354a5fa231556631
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-27 17:44:33 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,12 @@ def close_db_session(errpr):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """Handler for 404 errors"""
+    return ({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     HBNB_API_HOST = getenv("HBNB_API_HOST")
     HBNB_API_PORT = getenv("HBNB_API_PORT")
