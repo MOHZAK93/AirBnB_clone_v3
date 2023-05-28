436ec097778b3eae9a516e3508c746aaf1706b74
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-05-28 21:00:38 +0000

    fix

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -13,45 +13,46 @@ from models import storage
 @app_views.route('/amenities/<amenity_id>',
                  methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
 def amenities(amenity_id=None):
-    """Retrieves amenity objects"""
+    """Retrieves a list of amenity objects"""
+    amenity_objs = storage.all(Amenity)
 
-    amenity_objs = storage.all(Amenity).values()
-    amenities = [obj.to_dict() for obj in amenity_objs]
-
+    amenities = [obj.to_dict() for obj in amenity_objs.values()]
     if not amenity_id:
         if request.method == 'GET':
             return jsonify(amenities)
         elif request.method == 'POST':
             my_dict = request.get_json()
+
             if my_dict is None:
-                abort(400, "Not a JSON")
+                abort(400, 'Not a JSON')
             if my_dict.get("name") is None:
-                abort(400, "Missing name")
+                abort(400, 'Missing name')
             new_amenity = Amenity(**my_dict)
             new_amenity.save()
             return jsonify(new_amenity.to_dict()), 201
 
-        else:
-            if request.method == "GET":
-                for amenity in amenities:
-                    if amenity.get("id") == amenity_id:
-                        return jsonify(amenity)
-                abort(404)
-            elif request.method == "PUT":
-                my_dict = request.get_json()
-                if my_dict is None:
-                    abort(400, "Not a JSON")
-                for amenity in amenity_objs.values():
-                    if amenity.id == amenity_id:
-                        amenity.name = my_dict.get("name")
-                        amenity.save()
-                        return jsonify(amenity.to_dict()), 200
-                abort(404)
+    else:
+        if request.method == 'GET':
+            for amenity in amenities:
+                if amenity.get('id') == amenity_id:
+                    return jsonify(amenity)
+            abort(404)
+        elif request.method == 'PUT':
+            my_dict = request.get_json()
 
-            elif request.method == "DELETE":
-                for obj in amenity_objs.values():
-                    if obj.id == amenity_id:
-                        storage.delete(obj)
-                        storage.save()
-                        return jsonify({}), 200
-                abort(404)
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            for amenity in amenity_objs.values():
+                if amenity.id == amenity_id:
+                    amenity.name = my_dict.get("name")
+                    amenity.save()
+                    return jsonify(amenity.to_dict()), 200
+            abort(404)
+
+        elif request.method == 'DELETE':
+            for obj in amenity_objs.values():
+                if obj.id == amenity_id:
+                    storage.delete(obj)
+                    storage.save()
+                    return jsonify({}), 200
+            abort(404)
